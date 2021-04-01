aeb9266b6570b99c9c98b0d30fca65a472834411
srosanatalia <srosanatalia@gmail.com>
2021-03-31 22:26:31 -0300

    Integrando interface

--- src/Model/ClienteTCP.java
+++ src/Model/ClienteTCP.java
@@ -6,6 +6,7 @@
 package Model;
 
 import java.awt.HeadlessException;
+import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
@@ -26,7 +27,12 @@ public class ClienteTCP {
     public void leituraTag(){
         try {
             Socket cliente = new Socket(this.url, this.porta);
+            BufferedOutputStream saida = new BufferedOutputStream(cliente.getOutputStream());
             System.out.println("Conexão iniciada");
+            String example = "POST /rfid/config\\n{\"serial\":\"tmr:///dev/ttyUSB0\", \"baudrate\":\"115200\", \"region\":\"NA2\", \"protocol\":\"GEN2\", \"antenna\":\"1\", \"frequency\":\"1500\"}";
+            byte[] bytes = example.getBytes();
+            saida.flush();
+            saida.write(bytes);
           }
           catch(HeadlessException | IOException e) {
             System.out.println("Erro: " + e.getMessage());
