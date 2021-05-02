4556f508d60a6678c090ce6b7e5ef7addcdd7cb3
Laercio de Souza Rios <laercio.rios13@hotmail.com>
2021-05-02 16:51:59 -0300

    :books: Finalizado comentários no server

--- server/Consumer.py
+++ server/Consumer.py
@@ -1,3 +1,10 @@
+'''
+Classe resposável por gerar os resultados e enviar para o cliente.
+Funciona em dois modos:
+    - Qualificação
+    - Corrida
+'''
+
 import threading
 import time
 
@@ -16,14 +23,17 @@ class Consumer(threading.Thread):
         self.__consume()
 
     def __consume(self):
+        # Condicional para definir a mensagem de encerramento dependendo do modo
         if self.type == 'qualification':
             msg_finished = "QUALIFICATION_COMPLETED"
         elif self.type == 'race':
             msg_finished = "RACE_COMPLETED"
 
+        # Laço para esperar o buffer ser preenchido
         while self.buffer.is_empty():
             continue
 
+        # Pega primeiro dado do buffer que sempre será o tempo de inicio
         self.time_start = self.buffer.remove()
 
         data = ''
@@ -32,6 +42,7 @@ class Consumer(threading.Thread):
                 continue
 
             data = self.buffer.remove()
+            # Verifica se se é a mensagem de finalização 
             if data != msg_finished:
                 self.__update(data)
             else:
@@ -40,10 +51,13 @@ class Consumer(threading.Thread):
             print("QUALIFICATION COMPLETED")
         elif self.type == 'race':
             print("RACE COMPLETED")
+        # Envia para todos clientes conectados a mensagem de finalização
         for client in self.clients:
             time.sleep(1)
             client.clientsock.sendall(f'{msg_finished}!'.encode("utf-8"))
     
+    # Atualização das informações com base no modo, 
+    # sendo que após atualização ele gera um resultado e envia para o cliente
     def __update(self, data):
         if self.type == 'qualification':
             self.__update_qualification(data)
@@ -64,10 +78,11 @@ class Consumer(threading.Thread):
 
     def __get_result_qualification(self):
         result = []
-        best_time_qualification = None
+        best_time_qualification = None  # Tempo da melhor volta
         for car in self.cars:
             result_car = {'epc': car, 'best_time': None}
             for lap in self.cars[car]['laps']:
+                # Verifica se o tempo da volta é o melhor do carro
                 if (not result_car['best_time']) or (lap['time'] < result_car['best_time']):
                     result_car['best_time'] = lap['time']
                     if (not best_time_qualification) or (lap['time'] < best_time_qualification):
@@ -75,6 +90,7 @@ class Consumer(threading.Thread):
             if result_car['best_time'] != None:
                 result.append(result_car)
         
+        # Ordena os resultados e trata eles para envio e exibição do cliente
         result_order = sorted(result, key=lambda k: k['best_time'])
         for car in result_order:
             if car['best_time'] == best_time_qualification:
@@ -104,6 +120,7 @@ class Consumer(threading.Thread):
             result_car = {'epc': car, 'race_time': None, 'best_time': None, 'time_lap': None, 'laps': len(self.cars[car]['laps'])}
             for lap in self.cars[car]['laps']:
                 result_car['time_lap'] = lap['time']
+                # Soma o tempo da volta com o tempo que o carro já levou na corrida
                 if not result_car['race_time']:
                     result_car['race_time'] = lap['time']
                 else:
@@ -114,6 +131,7 @@ class Consumer(threading.Thread):
             if result_car['best_time'] != None:
                 result.append(result_car)
         
+        # Ordena os resultados e trata eles para envio e exibição do cliente
         result_order = multisort(list(result), (('laps', True), ('race_time', False)))
         for car in result_order:
             if (not best_time_race):
--- server/Producer.py
+++ server/Producer.py
@@ -1,3 +1,15 @@
+'''
+Classe resposável por manipular a leitura e o tratamento dos dados vindo do módulo RFID.
+Funciona em dois modos:
+    - Qualificação
+    - Corrida
+
+Nos dois casos o produtor manipula o tempo de leitura com base no tempo minimo de volta,
+isto é, o leitor não irá ler num intervalo de tempo impossível de haver leituras.
+Além de que é determinado um tempo máximo de qualificação/corrida,
+para caso algum carro não termine nem a primeira volta ou a corrida toda
+'''
+
 from datetime import datetime, timedelta
 import threading
 import sys
@@ -30,18 +42,20 @@ class Producer(threading.Thread):
             self.__start_race()
     
     def __start_qualification(self):
-        self.time_start = datetime.now()
-        self.time_end = self.time_start + timedelta(seconds=self.max_time_end)
-        self.time_next_read = self.time_start + timedelta(seconds=self.min_lap_time)
+        self.time_start = datetime.now()    # Tempo de inicio da qualificação
+        self.time_end = self.time_start + timedelta(seconds=self.max_time_end)  # Tempo para finalizar qualificação
+        self.time_next_read = self.time_start + timedelta(seconds=self.min_lap_time)    # Tempo minimo para uma próxima leitura
         self.buffer.add(self.time_start)
         for car in self.cars:
-            self.cars[car]['next_start_read'] = self.time_start + timedelta(seconds=self.min_lap_time)
-            self.cars[car]['first_read_lap'] = self.time_start
-        timeout = self.time_start + timedelta(seconds=self.max_time_end+self.min_lap_time)
-        while datetime.now() <= self.time_end:
+            self.cars[car]['next_start_read'] = self.time_start + timedelta(seconds=self.min_lap_time) # Tempo minimo para uma próxima leitura o carro
+            self.cars[car]['first_read_lap'] = self.time_start # Tempo da primeira leitura da volta
+        timeout = self.time_start + timedelta(seconds=self.max_time_end+self.min_lap_time) # Tempo limite para qualificação
+        while datetime.now() <= self.time_end: # Laço para qualificação ficar rodando enquanto estiver no tempo
+            # Laço para esperar até tempo minimo de próxima leitura para começar a ler
             while datetime.now() < self.time_next_read:
                 continue
             self.sensor.read_data(self.treat_data)
+            # Laço para esperar até que todos carros terminem a volta que iniciou ou até o limite máximo de qualificação
             while len(self.cars_ended_lap) != self.num_cars and datetime.now() < timeout:
                 continue
             self.sensor.stop_read_data()
@@ -61,6 +75,8 @@ class Producer(threading.Thread):
             while datetime.now() < self.time_next_read:
                 continue
             self.sensor.read_data(self.treat_data)
+            # Laço para esperar até que todos carros terminem a volta que iniciou ou até todos completarem a corrida
+            #  ou até o limite máximo de qualificação
             while len(self.cars_ended_lap) != self.num_cars and datetime.now() < timeout and len(self.cars_ended_race) != self.num_cars:
                 continue
             self.sensor.stop_read_data()
@@ -70,17 +86,22 @@ class Producer(threading.Thread):
 
     def treat_data(self, epc, rssi, timestamp):
         try:
+            # Verificação se a tag lida está na corrida
             if epc in self.cars.keys() and (type == 'qualification' or epc not in self.cars_ended_race):
+                # Verifica se a tag foi lida após o tempo minimo de volta 
                 if timestamp >= self.cars[epc]['next_start_read']:
                     self.cars[epc]['current_lap'] += 1
+                    # Define tempo minimo da próxima leitura
                     self.cars[epc]['next_start_read'] = timestamp + timedelta(seconds=self.min_lap_time)
+                    # Salva informações no buffer
                     self.buffer.add({"epc": epc, "timestamp": timestamp, "lap": self.cars[epc]['current_lap']})
-                    if self.cars[epc]['current_lap'] > self.current_lap:
+                    if self.cars[epc]['current_lap'] > self.current_lap: # Verificação para atualizar a volta atual da corrida
                         self.current_lap = self.cars[epc]['current_lap']
                         self.time_next_read = self.cars[epc]['next_start_read']
                 else:
                     if epc not in self.cars_ended_lap:
                         self.cars_ended_lap.append(epc)
+                    # Verifica se carro já fez todas voltas da corrida
                     if self.type == 'race' and epc not in self.cars_ended_race and self.cars[epc]['current_lap'] == self.num_laps:
                         self.cars_ended_race.append(epc)
         except Exception as e:
