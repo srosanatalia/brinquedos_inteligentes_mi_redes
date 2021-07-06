bb0d9e7e82697542f72e3fbe4a36c2f2de3895ac
Laercio de Souza Rios <laercio.rios13@hotmail.com>
2021-07-06 18:16:13 -0300

    :books: Adicionado comentários no server

--- server/client.py
+++ server/client.py
@@ -22,33 +22,35 @@ while not subscriber.has_new_message() and subscriber.get_topic_msg() != 'respon
 print(subscriber.get_message())
 time.sleep(5)
 
-# subscriber.set_topic('response/rfid/tags')
-# time.sleep(2)
-# subscriber.send_message('{"teste":true}'.encode("utf-8"), 'autorama/rfid/tags')
-# while not subscriber.has_new_message() and subscriber.get_topic_msg() != 'response/rfid/tags':
-#     continue
-# print(subscriber.get_message())
-# time.sleep(5)
+subscriber.set_topic('response/rfid/tags')
+time.sleep(2)
+# subscriber.send_message(''.encode("utf-8"), 'autorama/rfid/tags')
+subscriber.send_message('{"teste":true}'.encode("utf-8"), 'autorama/rfid/tags')
+while not subscriber.has_new_message() and subscriber.get_topic_msg() != 'response/rfid/tags':
+    continue
+print(subscriber.get_message())
+time.sleep(5)
 
 subscriber.set_topic('response/race/config')
 time.sleep(2)
-subscriber.send_message('{"min_time_speedway":"5", "max_time_qualification":"15", "num_laps_race":"3", "cars":["E2000017221101241890547C", "E20000172211012518905484"], "teste":true}'.encode("utf-8"), 'autorama/race/config')
+subscriber.send_message('{"min_time_speedway":"5", "max_time_qualification":"15", "num_laps_race":"3", "cars":["E2000017221101241890547C", "E20000172211012518905484"]}'.encode("utf-8"), 'autorama/race/config')
+# subscriber.send_message('{"min_time_speedway":"5", "max_time_qualification":"15", "num_laps_race":"3", "cars":["E2000017221101241890547C", "E20000172211012518905484"], "teste":true}'.encode("utf-8"), 'autorama/race/config')
 while not subscriber.has_new_message() and subscriber.get_topic_msg() != 'response/race/config':
     continue
 print(subscriber.get_message())
 time.sleep(5)
 
-# subscriber.set_topic('response/qualification')
-# time.sleep(2)
-# subscriber.send_message(''.encode("utf-8"), 'autorama/race/qualification/start')
-# while True:
-#     while not subscriber.has_new_message() and subscriber.get_topic_msg() != 'response/qualification':
-#         continue
-#     data = subscriber.get_message()
-#     print(data)
-#     if data == 'QUALIFICATION_COMPLETED!':
-#         break
-# time.sleep(5)
+subscriber.set_topic('response/qualification')
+time.sleep(2)
+subscriber.send_message(''.encode("utf-8"), 'autorama/race/qualification/start')
+while True:
+    while not subscriber.has_new_message() and subscriber.get_topic_msg() != 'response/qualification':
+        continue
+    data = subscriber.get_message()
+    print(data)
+    if data == 'QUALIFICATION_COMPLETED!':
+        break
+time.sleep(5)
 
 subscriber.set_topic('response/race')
 time.sleep(2)
